895bde09b84a5d27ad3b9f00bb1ea79856fc9fb0
baegteun <baegteun@gmail.com>
2022-06-06 23:15:59 +0900

    Initial Commit

---
+++ Shared/MinimalAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalAnimationApp.swift
+//  Shared
+//
+//  Created by 최형우 on 2022/06/06.
+//
+
+import SwiftUI
+
+@main
+struct MinimalAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
